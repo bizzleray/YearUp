c1fd5116efee8cee190bc3ad1a0e16167f6f5382
bizzleray <cbaines@PC286.student.YearUp.org>
2015-09-30 14:11:12 -0400

    My first file

---
+++ demoproject/src/demoproject/Intro.java
@@ -0,0 +1,11 @@
+package demoproject;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("chance baines");
+          System.out.println("My favorite book is cat in hat");
+          System.out.println("i like sports");
+	}
+}
